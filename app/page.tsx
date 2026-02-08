fef1a15b3b770e6c25959c8ee0752fe72582a4e4
abangsmails-jpg <abangsmails@gmail.com>
2026-02-08 14:52:15 +0100

    Create initial Home component in page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Home() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Acrid Content Pro</h1>
+      <p>Your AI app builder is ready to start!</p>
+    </div>
+  );
+}
